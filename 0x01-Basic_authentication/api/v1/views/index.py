38d3ebe3296a5bee0df2356e1167c4b403c18fad
Alae ben aicha <75539159+benaichalae@users.noreply.github.com>
2024-05-24 20:44:47 +0000

    py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,26 +1,44 @@
 #!/usr/bin/env python3
-""" Module of Index views
+"""Module of Index views.
 """
-from flask import jsonify, abort
+from flask import jsonify, abort, Blueprint
 from api.v1.views import app_views
 
+# Define a new Blueprint for the index views
+index_views = Blueprint('index_views', __name__)
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@index_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """GET /api/v1/status
     Return:
-      - the status of the API
+      - the status of the API.
     """
     return jsonify({"status": "OK"})
 
-
-@app_views.route('/stats/', strict_slashes=False)
+@index_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """GET /api/v1/stats
     Return:
-      - the number of each objects
+      - the number of each objects.
     """
     from models.user import User
-    stats = {}
-    stats['users'] = User.count()
+    stats = {
+        'users': User.count()
+    }
     return jsonify(stats)
+
+@index_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> None:
+    """GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error.
+    """
+    abort(401)
+
+@index_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> None:
+    """GET /api/v1/forbidden
+    Return:
+      - Forbidden error.
+    """
+    abort(403)
